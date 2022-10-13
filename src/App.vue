58867174a432f3fc95ef3219269aa785d9492e49
Alihan <alihandegirmenciler@gmail.com>
2022-10-13 18:47:03 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -16,24 +16,4 @@ import Results from './components/Results.vue';
 
 <style scoped>
 
-
-
-.track {
-  margin-top: 20px;
-}
-
-.horse {
-  display: flex;
-  height: 75px;
-}
-
-.finish {
-  @apply absolute right-0 w-2 h-[102px] bg-white mr-10
-}
-
-.line-wrapper {
-  background-color: rgba(37, 133, 32, 0.753);
-  border: 1px;
-  border-bottom: solid;
-}
 </style>
