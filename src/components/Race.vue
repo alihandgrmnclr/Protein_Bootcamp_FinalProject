70403d4973fae4aaf0fcf9f006e8f99f486b73a6
Alihan <alihandegirmenciler@gmail.com>
2022-10-13 18:47:09 +0300

    update

--- src/components/Race.vue
+++ src/components/Race.vue
@@ -68,4 +68,24 @@ const horseSelect = function () {
 
 <style scoped>
 
+
+.track {
+  margin-top: 20px;
+}
+
+.horse {
+  display: flex;
+  height: 75px;
+}
+
+.finish {
+  @apply absolute right-0 w-2 h-[102px] bg-white mr-10
+}
+
+.line-wrapper {
+  background-color: rgba(37, 133, 32, 0.753);
+  border: 1px;
+  border-bottom: solid;
+}
+
 </style>
