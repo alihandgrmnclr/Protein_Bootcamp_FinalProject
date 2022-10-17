a12fbaa20c2ad4c573983531107843df7c7712e0
Alihan <alihandegirmenciler@gmail.com>
2022-10-17 23:06:49 +0300

    bet and race seperated

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <script setup>
+import { ref } from "vue"
 import Race from './components/Race.vue';
 import Bet from './components/Bet.vue';
 
+const race = ref(false);
 
 </script>
 
 <template>
-  
-  <Bet></Bet>
-  <Race></Race>
+
+  <template v-if="!race">
+    <div class="bet">
+      <Bet></Bet>
+    </div>
+  </template>
+  <template v-else>
+    <Race></Race>
+  </template>
 
 </template>
 
 <style scoped>
 
-
+.bet{
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
 </style>
