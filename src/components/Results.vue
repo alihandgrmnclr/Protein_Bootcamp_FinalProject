961cc14a38cde5041aaecb9370f0b66260c4815e
Alihan <alihandegirmenciler@gmail.com>
2022-10-17 12:55:22 +0300

    draggable leaderboard added

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -5,8 +5,8 @@ const props = defineProps([]);
 
 // DRAGGABLE
 onMounted(() => {
-    const wrapper = document.querySelector(".wrapper");
-    const head = document.querySelector("#header");
+    const wrapper = document.querySelector(".wrapper"); // parent element of draggable div
+    const head = document.querySelector("#header"); // this is the draggable div
 
     function onDrag({ movementX, movementY }) {
         let getStyle = window.getComputedStyle(wrapper);
@@ -29,52 +29,52 @@ onMounted(() => {
 
 <template>
     <div class="leaderboard wrapper">
-        <header id="header">
+        <div id="header">
             <div class="leaderboard__header">
                 <img class="leaderboard__icon" src="https://cdn-icons-png.flaticon.com/512/744/744974.png" alt="">
                 <h1 class="leaderboard__title">LIDERLIK TABLOSU</h1>
             </div>
-            <div class="leaderboard__rankings">
-                <div class="leaderboard__profile">
-                    <span class="rank-first">1-</span> <span>Gülbatur</span> <span class="time">15.6<span
-                            class="seconds">saniye</span></span>
-                </div>
-                <div class="leaderboard__profile">
-                    <span class="rank-second">2-</span> <span>Şahbatur</span> <span class="time">15.6<span
-                            class="seconds">saniye</span></span>
-                </div>
-                <div class="leaderboard__profile">
-                    <span class="rank-third">3-</span> <span>Bold Pilot</span> <span class="time">15.6<span
-                            class="seconds">saniye</span></span>
-                </div>
-                <div class="leaderboard__profile">
-                    <span class="rank">4-</span> <span>Black Lightning</span> <span class="time">15.6<span
-                            class="seconds">saniye</span></span>
-                </div>
-                <div class="leaderboard__profile">
-                    <span class="rank">5-</span> <span>Mucit Tay</span> <span class="time">15.6<span
-                            class="seconds">saniye</span></span>
-                </div>
-                <div class="leaderboard__profile">
-                    <span class="rank">6-</span> <span>Barbaros</span> <span class="time">15.6<span
-                            class="seconds">saniye</span></span>
-                </div>
-                <div class="leaderboard__profile">
-                    <span class="rank">7-</span> <span>Manas</span> <span class="time">15.6<span
-                            class="seconds">saniye</span></span>
-                </div>
-                <div class="leaderboard__profile">
-                    <span class="rank">8-</span> <span>Sipahi</span> <span class="time">15.6<span
-                            class="seconds">saniye</span></span>
-                </div>
+        </div>
+        <div class="leaderboard__rankings">
+            <div class="leaderboard__profile">
+                <span class="rank-first">1-</span> <span>Gülbatur</span> <span class="time">15.6<span
+                        class="seconds">saniye</span></span>
             </div>
-        </header>
+            <div class="leaderboard__profile">
+                <span class="rank-second">2-</span> <span>Şahbatur</span> <span class="time">15.6<span
+                        class="seconds">saniye</span></span>
+            </div>
+            <div class="leaderboard__profile">
+                <span class="rank-third">3-</span> <span>Bold Pilot</span> <span class="time">15.6<span
+                        class="seconds">saniye</span></span>
+            </div>
+            <div class="leaderboard__profile">
+                <span class="rank">4-</span> <span>Black Lightning</span> <span class="time">15.6<span
+                        class="seconds">saniye</span></span>
+            </div>
+            <div class="leaderboard__profile">
+                <span class="rank">5-</span> <span>Mucit Tay</span> <span class="time">15.6<span
+                        class="seconds">saniye</span></span>
+            </div>
+            <div class="leaderboard__profile">
+                <span class="rank">6-</span> <span>Barbaros</span> <span class="time">15.6<span
+                        class="seconds">saniye</span></span>
+            </div>
+            <div class="leaderboard__profile">
+                <span class="rank">7-</span> <span>Manas</span> <span class="time">15.6<span
+                        class="seconds">saniye</span></span>
+            </div>
+            <div class="leaderboard__profile">
+                <span class="rank">8-</span> <span>Sipahi</span> <span class="time">15.6<span
+                        class="seconds">saniye</span></span>
+            </div>
+        </div>
+
     </div>
 
 </template>
 
 <style lang="scss" scoped>
-
 .leaderboard {
     opacity: .7;
     @apply flex flex-col justify-center items-center p-3 bg-gray-800 text-slate-50 w-96 rounded-xl absolute z-10 cursor-move;
@@ -82,7 +82,7 @@ onMounted(() => {
     font-family: 'Source Sans Pro', sans-serif;
 
     &__header {
-        @apply flex flex-col justify-center items-center;
+        @apply flex flex-col justify-center items-center w-full;
     }
 
     &__icon {
