45417dd28763835afeb1d9caacd606dc533fa629
Alihan <alihandegirmenciler@gmail.com>
2022-10-28 18:23:13 +0300

    modal css update

--- src/components/Bet/BetModal.vue
+++ src/components/Bet/BetModal.vue
@@ -21,7 +21,8 @@ const declineBet = () => {
     <template v-if="props.isModalDisplay">
       <div class="alert">
         <div class="alert__wrapper">
-          <div class="alert__description">
+          <div class="alert__content">
+            <div class="alert__description">
             <p>Bet Amount: <b>{{ props.bet }}$</b></p>
             <p>Selected Horse: <b>{{ props.selected }}</b> </p>
             <p>Do you agree to accept bet?</p>
@@ -29,6 +30,7 @@ const declineBet = () => {
           <div class="alert__buttons">
             <ButtonComp @click="acceptBet(), clickSound()" :text="'Accept'"></ButtonComp>
             <ButtonComp @click="declineBet(), clickSound()" :text="'Decline'"></ButtonComp>
+          </div>
           </div>
         </div>
       </div>
@@ -38,10 +40,16 @@ const declineBet = () => {
 
 <style lang="scss" scoped>
 .alert {
-  @apply absolute left-0 right-0 ml-auto mr-auto mt-[40vh] w-[350px] h-[200px] bg-zinc-300;
+  @apply absolute left-0 right-0 ml-auto mr-auto w-full h-full;
+  background: rgba(89, 88, 88, 0.931);
 
   &__wrapper {
     @apply flex flex-col items-center justify-center h-full w-full;
+  }
+
+  &__content{
+    @apply flex flex-col justify-center w-[300px] h-[250px] rounded-lg bg-bgsecondary;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
   }
 
   &__description {
